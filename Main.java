4d890ffc6e53eedd848429fc8a768b0e25fbde38
omdotcode08 <ommanglani88@gmail.com>
2024-10-21 14:02:02 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,4 @@ public class Main {
         System.out.println("Hello, Jenkins!");
     }
 }
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
-    }
-}
+
